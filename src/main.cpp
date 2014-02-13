b79ab24bf9478e3a0047ae3fead2803e65383e7d
patrick <github@p.jpschaaf.com>
2014-02-12 22:42:13 -0600

    Added walls around the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "sdl/Game.h"
 #include "World.h"
 #include "Car.h"
+#include "Wall.h"
+#include "Point.h"
 
 int main(int argc, char **argv)
 {
     Game game;
     World world;
     world.Cars().push_back(std::move(Car()));
+    world.Walls().push_back(std::move(Wall(Point(0,0), Point(0,480))));
+    world.Walls().push_back(std::move(Wall(Point(0,480), Point(640,480))));
+    world.Walls().push_back(std::move(Wall(Point(640,480), Point(640,0))));
+    world.Walls().push_back(std::move(Wall(Point(640,0), Point(0,0))));
+    
     while (game.Running()) {
         game.UpdateFrame(world);
     }
